7294a3062dd635dea8d0864c4a3bb0e5de462a1d
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-01-12 23:59:28 -0700

    finally fixed janky buttons

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -7,11 +7,11 @@ const WorkSliderBtns = ({ containerStyles, btnStyles, iconsStyles }) => {
 	const swiper= useSwiper();
   return (
     <div className={containerStyles}>
-      <button className={btnStyles}>
-        <PiCaretLeftBold className={iconsStyles} onClick={()=> swiper.slidePrev() } />
+      <button className={btnStyles} onClick={()=> swiper.slidePrev() } >
+        <PiCaretLeftBold className={iconsStyles} />
       </button>
-      <button className={btnStyles}>
-        <PiCaretRightBold className={iconsStyles} onClick={()=> swiper.slideNext() } />
+      <button className={btnStyles}onClick={()=> swiper.slideNext() } >
+        <PiCaretRightBold className={iconsStyles} />
       </button>
     </div>
   );
